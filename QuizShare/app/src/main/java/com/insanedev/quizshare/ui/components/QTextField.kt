4114d240d2ffe4dd036fa106604af3fea2624677
Alex Mit <71551755+d1agnozzz@users.noreply.github.com>
2022-08-15 04:19:49 +0300

    keyboard adjustments on a sign in screen

--- QuizShare/app/src/main/java/com/insanedev/quizshare/ui/components/QTextField.kt
+++ QuizShare/app/src/main/java/com/insanedev/quizshare/ui/components/QTextField.kt
@@ -1,6 +1,8 @@
 package com.insanedev.quizshare.ui.components
 
-import androidx.compose.material.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -15,15 +17,18 @@ import com.insanedev.quizshare.ui.theme.AppTheme
 
 @Composable
 fun QTextField(
-    modifier: Modifier = Modifier,
     value: String,
     placeholder: String,
     secureText: Boolean = false,
-    onTextFieldChange: (String) -> Unit
-) {
+    enabled: Boolean = true,
+    onTextFieldChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default.copy(),
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+
+    ) {
     OutlinedTextField(
         modifier = modifier,
-
         value = value,
         placeholder = {
             Text(
@@ -40,8 +45,12 @@ fun QTextField(
             focusedBorderColor = AppTheme.colors.primaryAccent
 
         ),
+        enabled = enabled,
+        singleLine = true,
         visualTransformation = if (secureText) PasswordVisualTransformation() else VisualTransformation.None,
-        onValueChange = onTextFieldChange
+        onValueChange = onTextFieldChange,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
     )
 }
 
